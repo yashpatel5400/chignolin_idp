75bc08f05d0646ea9179bce9fb2cba0800c8b90e
yashpatel5400 <ypatel5400@gmail.com>
2023-04-24 15:59:27 -0400

    Replicated instance seems to work just fine

--- replicator.py
+++ replicator.py
@@ -1,18 +1,20 @@
 # makes artificial replicates of PDB
 
-import parmed as pmd
 import copy
+import parmed as pmd
+import numpy as np
 import openmm.app as app
 
 fn = "/home/yppatel/misc/chignolin_idp/disordered_chignolin_eval/GYDPETGTWG.pdb"
 pdb = pmd.load_file(fn)
 
-num_replicates = 2
+num_replicates = 25
 
+pdb_positions = np.array(pdb.positions._value)
 pdbs = []
 for n in range(num_replicates):
     pdb_offset = copy.deepcopy(pdb)
-    pdb_offset.positions *= (1 + .05 * n)
+    pdb_offset.positions = pdb_positions + 100 * n
     pdbs.append(pdb_offset)
 
 combined_pdb_fn = "combined.pdb"
